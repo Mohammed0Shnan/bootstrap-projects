531e4cd79361028f0b0d01326435df9bbe92648c
Mohammed0Shnan <mohammed0shnan@gmail.com>
2023-03-30 01:47:40 -0700

    first-commit

---
+++ js/app.js
@@ -0,0 +1,54 @@
+    // $(document).ready(function(){
+    //     $('#hero-slider').owlCarousel({
+    //         loop:true,
+    //         margin:10,
+    //         nav:true,
+    //         items:1,
+    //         responsive:{
+    //             0:{
+    //             },
+    //             600:{
+    //             },
+    //             1000:{
+    //             }
+    //         }
+    //     });
+
+
+    //     $('#hero2').owlCarousel({
+    //         loop:true,
+    //         margin:10,
+    //         nav:true,
+    //        smartSpeed:1000,
+    //        navText:['P','N'],
+    //        dots:true,
+    //         responsive:{
+    //             0:{
+    //                 items:1,
+    //                 nav:true
+    //             },
+    //             600:{
+    //                 items:2,
+    //                 nav:true
+    //             },
+    //             1140:{
+    //                 items:2,
+    //                 center:true,
+    //                 nav:false
+    //             }
+    //         }
+    //     });
+    // });
+
+
+    // AOS
+
+      AOS.init({
+        offset :120,
+        delay:0,
+        duration:1500,
+        easing:'ease',
+        once:false,
+        mirror:false,
+        anchorPlacement:'top-bottom'
+      });
